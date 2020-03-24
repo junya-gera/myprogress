8d8c1589f6b9665d1207d125a47cba295bdcb803
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-24 13:50:37 +0900

    add bars using SFC

---
+++ app/javascript/bars.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <i class="fas fa-bars fa-lg"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import App from '../app.vue'
+import Bars from '../bars.vue'
+
+Vue.config.productionTip = false;
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
+
+  new Vue({
+    render: h => h(Bars),
+    el: '#bars'
+  })
+
 
   console.log(app)
 })
